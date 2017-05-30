146eb4736676cf1883a3829d1cdc12ecbe5bf883
Nikolai Elich <6elich@informatik.uni-hamburg.de>
2017-05-30 19:19:08 +0200

    Füge die Funktion "put" hinzu
    Diese Funktion nimmt als Parameter ein gameboard und den Index der
    Lane in die ein Spielstein eingeworfen werden soll.
    Sie liefert ein gameboard bei dem ein Spielstein in die angegebene
    Lane eingeworfen wurde.
    Falls die angegebene Lane schon voll ist, liefert die Funktion NULL.

--- vier-gewinnt.c
+++ vier-gewinnt.c
@@ -5,3 +5,24 @@ struct gameboard
     int isFinished;
     int nextPlayer;
 };
+
+struct gameboard put(struct gameboard current, int laneIndex)
+{
+    struct gameboard new = current;
+    int *updateLane = new.lanes[laneIndex];
+    int laneSize = sizeof(updateLane)/sizeof(updateLane[0]);
+    int rowIndex = 0;
+
+    while(rowIndex < laneSize && updateLane[rowIndex] == 0)
+    {
+        rowIndex += 1;
+    }
+
+    if (rowIndex > 0)
+    {
+        updateLane[rowIndex - 1] = new.nextPlayer;
+        new.nextPlayer = 2 - (2 % new.nextPlayer);
+        return new;
+    }
+    return NULL;
+}
