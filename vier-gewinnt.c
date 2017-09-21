82d1c68d01295067fec7cca7d03d8f7332760864
Unknown <6elich@informatik.uni-hamburg.de>
2017-09-21 18:32:49 +0200

    Add function to print prompt for player

--- vier-gewinnt.c
+++ vier-gewinnt.c
@@ -1,10 +1,67 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
 #include "createParallelTree.h"
 #include "gameboard.h"
 #include "knot.h"
 
+extern const int boardWidth, boardHeight;
+
 struct knot playerKnot;
+
+void printPlayerPrompt()
+{
+    printf(" ");
+    for (int i = 0; i < boardWidth; i++)
+    {
+        printf(" %d  ", i);
+    }
+    printf("\n");
+
+    printf("┌───");
+    for (int i = 1; i < boardWidth; i++)
+    {
+        printf("┬───");
+    }
+    printf("┐\n");
+
+    for (int rowIndex = 0; rowIndex < boardHeight; rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < boardWidth; columnIndex++)
+        {
+            char boardPiece = ' ';
+            if (playerKnot.gameboard->lanes[columnIndex][rowIndex] == 1)
+            {
+                boardPiece = 'O';
+            }
+            else if (playerKnot.gameboard->lanes[columnIndex][rowIndex] == 2)
+            {
+                boardPiece = 'X';
+            }
+            printf("│ %c ", boardPiece);
+        }
+        printf("│\n");
+
+        if (rowIndex == boardHeight - 1)
+        {
+            break;
+        }
+        printf("├───");
+        for (int columnIndex = 1; columnIndex < boardWidth; columnIndex++)
+        {
+            printf("┼───");
+        }
+        printf("┤\n");
+    }
+
+    printf("└───");
+    for (int i = 1; i < boardWidth; i++)
+    {
+        printf("┴───");
+    }
+    printf("┘\n");
+    printf("Enter where to put the next 'O':\n");
+}
 
 int main(int argc, char *argv[])
 {
