1fdbcfe29fba17927ebde1fe26690027806b54a8
Unknown <6elich@informatik.uni-hamburg.de>
2017-10-21 12:08:16 +0200

    Make turnSizes and turnDisplacements extern

--- createParallelTree.h
+++ createParallelTree.h
@@ -4,6 +4,9 @@
 #include "gameboard.h"
 #include "knot.h"
 
+extern int turnSizes[(BOARD_WIDTH * BOARD_HEIGHT) + 1];
+extern int turnDisplacements[(BOARD_WIDTH * BOARD_HEIGHT) + 1];
+
 void buildParallelTree(struct knot *startKnot,
                        struct gameboard *startGameboard, struct knot **((*turnsPointer)[(BOARD_WIDTH * BOARD_HEIGHT) + 1]));
 
