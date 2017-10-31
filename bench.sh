b10b6a0f7a18882fc1aa657d71e1c4039ee96d4f
Unknown <6elich@informatik.uni-hamburg.de>
2017-10-31 21:03:04 +0100

    Add benchmark scripts

---
+++ bench.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for (( i = 4; i > 1; i-- )); do
+    export PROCESS_NUMBER=$i
+    for (( j = 2; j <= 4; j++ )); do
+        for (( k = 2; k <= 4; k++ )); do
+            export BOARD_WIDTH=$j BOARD_HEIGHT=$k
+            make -B
+            logFileName="./logs/${j}x$k-$i"
+            rm -f $logFileName
+            for (( l = 0; l < 5; l++ )); do
+                sbatch -o job-$l.out bench.slurm
+            done
+
+            jobsDone="False"
+            until [[ "$jobsDone" == "" ]]; do
+                jobsDone=$(squeue | grep elich)
+                sleep 10
+            done
+
+            for (( l = 0; l < 5; l++ )); do
+                cat job-$l.out >> $logFileName
+            done
+        done
+    done
+done
